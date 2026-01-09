f35623841fd3c8d860b70c6f534c5ceba67e119b
Bintang <nbintangh@gmail.com>
2026-01-10 05:24:38 +0700

    feat: change wwarnf to fatalf

--- cmd/seed/main.go
+++ cmd/seed/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"rest-fiber/config"
 	"rest-fiber/internal/infra"
 	"strconv"
@@ -12,23 +11,24 @@ import (
 
 func main() {
 	dbLogger := infra.NewDBLogger()
+
 	env, err := config.GetEnvs()
 	if err != nil {
-		logrus.Warnf("Seed failed: %v", err)
+		logrus.Fatalf("Seed failed: %v", err)
 	}
+
 	db, err := infra.GetDatabaseStandalone(env, dbLogger)
 	if err != nil {
-		logrus.Warnf("Seed failed: %v", err)
+		logrus.Fatalf("Seed failed: %v", err)
 	}
+
 	countFlag := flag.String("count", "1", "specify the count")
 	flag.Parse()
-	countStr := *countFlag
-	count, err := strconv.Atoi(countStr)
+
+	count, err := strconv.Atoi(*countFlag)
 	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		logrus.Fatalf("Invalid count: %v", err)
 	}
-	InitSeeds(db, Options{
-		Count: count,
-	})
+
+	InitSeeds(db, Options{Count: count})
 }
